e9d8d30a77475721ac0211c66ce3152b63397388
samuel severance <sam@samuels-MacBook-Pro.local>
2016-07-12 18:30:08 -0400

    updates

---
+++ browser/app/signin/singin.directive.js
@@ -0,0 +1,45 @@
+'use strict';
+
+app.directive('signIn', function ($http, $log, Auth, $state) {
+  return {
+    restrict: 'E',
+    templateUrl: '/browser/app/signin/sing-in.html',
+    scope: {
+      usermethod: "="
+    },
+    link: function (scope, element, attrs) {
+
+      var fnToCall = usermethod === 'login' ? Auth.login : Auth.signup;
+
+      scope.signupSubmit = function () {
+        var userInfo = {
+          email: scope.email,
+          password: scope.password
+        };
+
+        fnToCall(userInfo)
+        .then(function(response){
+          $state.go("stories")
+          console.log("signup successful!")
+        })
+        .catch($log.error);
+      }
+
+      // scope.loginSubmit = function() {
+
+      //   var userInfo = {
+      //     email: scope.email,
+      //     password: scope.password
+      //   };
+
+      //   Auth.login(userInfo)
+      //   .then(function(response){
+      //     $state.go("stories")
+      //     console.log("login successful!")
+      //   })
+      //   .catch($log.error);
+      // }
+
+    }
+  };
+});
